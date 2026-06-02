8eed55125b166545f3f4da7fb7592177aabb3da7
e4je <e4je@users.noreply.github.com>
2026-06-02 12:31:30 +0800

    Remove redundant GORM index declarations

--- model/topup.go
+++ model/topup.go
@@ -16,7 +16,7 @@ type TopUp struct {
 	UserId          int     `json:"user_id" gorm:"index"`
 	Amount          int64   `json:"amount"`
 	Money           float64 `json:"money"`
-	TradeNo         string  `json:"trade_no" gorm:"unique;type:varchar(255);index"`
+	TradeNo         string  `json:"trade_no" gorm:"unique;type:varchar(255)"`
 	PaymentMethod   string  `json:"payment_method" gorm:"type:varchar(50)"`
 	PaymentProvider string  `json:"payment_provider" gorm:"type:varchar(50);default:''"`
 	CreateTime      int64   `json:"create_time"`
